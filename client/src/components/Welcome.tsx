c9c9c4325d120d5b04a3ae3f460e3cb40990c277
Luis Locon <luis.locon@gmail.com>
2024-08-22 00:42:22 -0600

    Adding welcome and Loader components

--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -7,6 +7,33 @@ import { Loader } from "./";
 const commonStyles =
   "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
+interface InputProps {
+  placeHolder: string;
+  name: string;
+  value: string | number;
+  type: string;
+  handleChange: () => void;
+}
+const Input = ({
+  placeHolder,
+  name,
+  type,
+  value,
+  handleChange,
+}: InputProps) => {
+  return (
+    <input
+      placeholder={placeHolder}
+      type={type}
+      value={value}
+      name={name}
+      step={0.01}
+      onChange={handleChange}
+      className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+    />
+  );
+};
+
 const Welcome = () => {
   const connectWallet = (
     event: MouseEvent<HTMLButtonElement, MouseEvent>
@@ -14,10 +41,16 @@ const Welcome = () => {
     throw new Error("Function not implemented.");
   };
 
+  function handleSubmit(
+    event: MouseEvent<HTMLButtonElement, MouseEvent>
+  ): void {
+    throw new Error("Function not implemented.");
+  }
+
   return (
     <div className="flex w-full justify-center items-center">
-      <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-        <div className="flex flex-1 justify-start flex-col md:mr-10">
+      <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-start flex-col mf:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
             Send Crypto <br /> across the world
           </h1>
@@ -39,6 +72,67 @@ const Welcome = () => {
             <div className={`rounded-bl-2xl ${commonStyles}`}>Web 3.0</div>
             <div className={` ${commonStyles}`}>Low fees</div>
             <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
+          </div>
+        </div>
+
+        <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
+          <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
+            <div className="flex justify-between flex-col w-full h-full">
+              <div className="flex justify-between items-start">
+                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                  <SiEthereum fontSize={21} color="#FFF" />
+                </div>
+                <BsInfoCircle fontSize={17} color="#FFF" />
+              </div>
+              <div>
+                <p className="text-white font-light text-sm">Address</p>
+                <p className="text-white font-semibold text-lg mt-1">
+                  Ethereum
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+            <Input
+              type="text"
+              value={""}
+              handleChange={() => {}}
+              name="AddressTo"
+              placeHolder="Address To"
+            />
+            <Input
+              type="number"
+              value={""}
+              handleChange={() => {}}
+              name="amount"
+              placeHolder="Amount (ETH)"
+            />
+            <Input
+              type="text"
+              value={""}
+              handleChange={() => {}}
+              name="keyword"
+              placeHolder="Keyword (GIF)"
+            />
+            <Input
+              type="text"
+              value={""}
+              handleChange={() => {}}
+              name="message"
+              placeHolder="Enter Message"
+            />
+            <div className="h-[1px] w-full bg-gray-400 my-2" />
+            {false ? (
+              <Loader />
+            ) : (
+              <button
+                type="button"
+                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"
+                onClick={handleSubmit}
+              >
+                Send Now
+              </button>
+            )}
           </div>
         </div>
       </div>
